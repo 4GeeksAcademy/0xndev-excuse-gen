68ecef350d77261df27fb4ed4475d2e3d6e9afeb
0xndev <141341761+0xndev@users.noreply.github.com>
2023-11-07 01:17:24 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
 };
 let generateExcuse = () => {
